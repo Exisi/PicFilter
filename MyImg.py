8e5cf2de21e295a5a1731f15c18065f07efa7dd3
未默初蚀 <64684842+Exisi@users.noreply.github.com>
2021-07-12 19:34:28 +0800

    横竖图片分类

---
+++ MyImg.py
@@ -0,0 +1,27 @@
+import imghdr
+from PIL import Image
+
+def is_img(f):
+    #判断文件是否为图片
+    '''
+    :param f: 图片路径
+    :param imgType_list: 图片格式
+    :return: bool
+    '''
+    imgType_list = {'jpg', 'bmp', 'png', 'jpeg', 'rgb', 'tif', 'gif', 'webp'}
+    if imghdr.what(f) in imgType_list:
+        return True
+    else:
+        return False
+
+def wh_type(f):
+    #判断图片是横屏还是竖屏
+    '''
+    :param f: 图片路径
+    :return: bool
+    '''
+    im = Image.open(f)
+    if im.width>im.height: #横屏
+        return True
+    else:                  #竖屏
+        return False
---
+++ start.py
@@ -0,0 +1,31 @@
+import Function
+import MyImg
+
+output_path="D:\ChromeCoreDownloads"
+
+def im_classify(f):
+    if MyImg.wh_type(f):
+        Function.copy(f,file_output=output_path,mode=1)
+    else:
+        Function.copy(f, file_output=output_path, mode=0)
+    print(Function.name(f))
+
+def main():
+    #载入图片
+    file_dir=input("请输入图片目录：")
+    output_path=input("请输入图片输出目录：")
+    if Function.exists(file_dir):
+        print("文件已读取")
+        Function.create_folder(output_path)
+        file_list=Function.read(file_dir)
+        for f in file_list:
+
+            if MyImg.is_img(f):
+                im_classify(f)
+
+        input("处理完成，输入任意键退出")
+    else:
+        print("文件路目录不存在!")
+
+if __name__ == "__main__":
+    main()
