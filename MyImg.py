54d069f44f6e516d6397efde3ba3cbbfcc729eef
Exisi <64684842+Exisi@users.noreply.github.com>
2021-08-02 21:14:09 +0800

    V0.2 Update 图片处理

--- MyImg.py
+++ MyImg.py
@@ -1,6 +1,5 @@
 import imghdr
 from PIL import Image
-import hashlib
 
 
 def is_img(f):
@@ -31,7 +30,12 @@ def wh_type(f):
 
 
 def hist(f):
-    im=Image.open(f)
-    size=10,10  #不建议调低，容易误判，大小像素最低值5
-    i=im.resize(size, Image.ANTIALIAS).convert('RGB')
+    '''
+    :param f: 图片路径
+    :param size: 图片重设的大小，减小像素计算量
+    :return: Pil.im.histogram 图片的像素直方图
+    '''
+    im = Image.open(f)
+    size = 10, 10  # 不建议调低，容易误判，大小像素最低值5
+    i = im.resize(size, Image.ANTIALIAS)
     return i.histogram()
